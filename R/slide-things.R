acb0c8165f15e5ed609a90c7405c11857ea85194
Andrew Heiss <andrew@andrewheiss.com>
2023-05-23 07:51:25 -0400

    Add all content pages

---
+++ R/slide-things.R
@@ -0,0 +1,14 @@
+slide_links <- function(pdf = NULL, thumb = NULL) {
+  if (is.null(pdf)) {
+    out <- "<p>The slides from today will be posted here.</p>"
+  } else {
+    out <- glue::glue(
+      '<p class="buttons"><a class="btn btn-danger" target="_blank" href="{pdf}" role="button"><i class="fa-solid fa-file-pdf"></i> Download PDF of all slides</a></p>',
+      '<figure class="slide-thumb">',
+      '<a href="{pdf}"><img class="img-fluid" src="{thumb}" alt="First slide" /></a>',
+      "</figure>"
+    )
+  }
+  
+  return(out)
+}
