a1ae2d180c3ec3ff33a5f90ee5199149ed40245d
Andrew Heiss <andrew@andrewheiss.com>
2023-05-23 21:48:10 -0400

    Get schedule working

--- R/tar_calendar.R
+++ R/tar_calendar.R
@@ -19,75 +19,88 @@ build_schedule_for_page <- function(schedule_file) {
     # to character. So here I add the deadline time to the deadline date with
     # update() and then format it with format.Date()
     mutate(deadline_actual = update(date, hour = hour(deadline), minute = minute(deadline)),
-           deadline_nice = format(deadline_actual, "%I:%M %p")) %>%
+      deadline_nice = format(deadline_actual, "%I:%M %p")) %>%
     mutate(group = fct_inorder(group)) %>%
     mutate(subgroup = fct_inorder(subgroup)) %>%
     mutate(var_note = ifelse(!is.na(note),
-                             glue('<br><span class="class-note">{note}</span>'),
-                             glue(""))) %>%
-    mutate(var_title = ifelse(!is.na(class),
-                              glue('<span class="class-title">{title}</span>'),
-                              glue('{title}'))) %>%
+      glue('<br><span class="content-note">{note}</span>'),
+      glue(""))) %>%
+    mutate(var_flag_start = ifelse(!is.na(flag) & flag == TRUE, '<span class="content-flag"><i class="fas fa-star"></i>&ensp;', "")) %>% 
+    mutate(var_flag_end = ifelse(!is.na(flag) & flag == TRUE, "</span>", "")) %>% 
+    mutate(var_title = ifelse(!is.na(content),
+      glue('<span class="content-title">{var_flag_start}{title}{var_flag_end}</span>'),
+      glue('{var_flag_start}{title}{var_flag_end}'))) %>%
     mutate(var_deadline = ifelse(!is.na(deadline),
-                                 glue('&emsp;&emsp;<small>(submit by {deadline_nice})</small>'),
-                                 glue(""))) %>%
-    mutate(var_class = ifelse(!is.na(class),
-                              glue('<a href="{class}.qmd"><i class="fa-solid fa-book-open-reader fa-lg"></i></a>'),
-                              glue('<font color="#e9ecef"><i class="fa-solid fa-book-open-reader fa-lg"></i></font>'))) %>%
+      glue('&emsp;&emsp;<small>(submit by {deadline_nice})</small>'),
+      glue(""))) %>%
+    mutate(var_content = ifelse(!is.na(content),
+      glue('<a href="{content}.qmd"><i class="fa-solid fa-book-open-reader fa-lg"></i></a>'),
+      glue('<font color="#e9ecef"><i class="fa-solid fa-book-open-reader fa-lg"></i></font>'))) %>%
     mutate(var_assignment = ifelse(!is.na(assignment),
-                                   glue('<a href="{assignment}.qmd"><i class="fa-solid fa-pen-ruler fa-lg"></i></a>'),
-                                   glue('<font color="#e9ecef"><i class="fa-solid fa-pen-ruler fa-lg"></i></font>'))) %>%
-    mutate(col_date = glue('<strong>{format(date, "%B %e")}</strong>')) %>%
+      glue('<a href="{assignment}.qmd"><i class="fa-solid fa-pen-ruler fa-lg"></i></a>'),
+      glue('<font color="#e9ecef"><i class="fa-solid fa-pen-ruler fa-lg"></i></font>'))) %>%
+    mutate(date_range = ifelse(!is.na(end_date),
+      glue('{format(date, "%B %e")}–{format(end_date, "%B %e")}'), NA)) %>% 
+    mutate(date_range_group = consecutive_id(date_range)) %>%
+    group_by(date_range_group) %>% 
+    mutate(date_range_official = ifelse(row_number() == 1, date_range, NA)) %>% 
+    ungroup() %>% 
+    mutate(col_date = case_when(
+      !is.na(content) ~ ifelse(is.na(date_range_official), 
+        glue('<span class="content-session">({session})</span>'), 
+        glue('<span class="content-date">{date_range_official}</span><br><span class="content-session">({session})</span>')),
+      TRUE ~ glue('<span class="content-date">{format(date, "%B %e")}</span>')
+    )) %>% 
     mutate(col_title = glue('{var_title}{var_deadline}{var_note}')) %>%
-    mutate(col_class = var_class,
-           col_assignment = var_assignment)
-
+    mutate(col_content = var_content,
+      col_assignment = var_assignment)
+  
   schedule_nested <- schedule %>%
     select(group, subgroup,
-           ` ` = col_date, Title = col_title,
-           Class = col_class, Assignment = col_assignment) %>%
+      ` ` = col_date, Title = col_title,
+      Class = col_content, Assignment = col_assignment) %>%
     group_by(group) %>%
     nest() %>%
     mutate(subgroup_count = map(data, ~count(.x, subgroup)),
-           subgroup_index = map(subgroup_count, ~{
-             .x %>% pull(n) %>% set_names(.x$subgroup)
-           }))
-
+      subgroup_index = map(subgroup_count, ~{
+        .x %>% pull(n) %>% set_names(.x$subgroup)
+      }))
+  
   return(schedule_nested)
 }
 
 # Read the schedule CSV file and create a dataset formatted as iCal data that
 # calendar::ic_write() can use
-build_ical <- function(schedule_file, base_url, page_suffix, class_number) {
+build_ical <- function(schedule_file, base_url, page_suffix, content_number) {
   dtstamp <- ic_char_datetime(now("UTC"), zulu = TRUE)
-
+  
   schedule <- read_csv(schedule_file, show_col_types = FALSE) %>%
-    mutate(session = if_else(is.na(class), glue(""), glue("({subgroup}) "))) %>%
-    mutate(summary = glue("{class_number}: {session}{title}")) %>%
+    mutate(session = if_else(is.na(content), glue(""), glue("({subgroup}) "))) %>%
+    mutate(summary = glue("{content_number}: {session}{title}")) %>%
     mutate(date_start_dt = date,
-           date_end_dt = date) %>%
+      date_end_dt = date) %>%
     mutate(date_start_cal = map(date_start_dt, ~as.POSIXct(., format = "%B %d, %Y")),
-           date_end_cal = map(date_end_dt, ~as.POSIXct(., format = "%B %d, %Y"))) %>%
-    mutate(url = coalesce(class, assignment),
-           url = if_else(is.na(url), glue(""), glue("{base_url}{url}{page_suffix}")))
-
+      date_end_cal = map(date_end_dt, ~as.POSIXct(., format = "%B %d, %Y"))) %>%
+    mutate(url = coalesce(content, assignment),
+      url = if_else(is.na(url), glue(""), glue("{base_url}{url}{page_suffix}")))
+  
   schedule_ics <- schedule %>%
     mutate(id = row_number()) %>%
     group_by(id) %>%
     nest() %>%
     mutate(ical = map(data,
-                      ~ic_event(start = .$date_start_cal[[1]],
-                                end = .$date_end_cal[[1]] + 24*60*60,
-                                summary = .$summary[[1]],
-                                more_properties = TRUE,
-                                event_properties = c("DESCRIPTION" = .$url[[1]],
-                                                     "DTSTAMP" = dtstamp)))) %>%
+      ~ic_event(start = .$date_start_cal[[1]],
+        end = .$date_end_cal[[1]] + 24*60*60,
+        summary = .$summary[[1]],
+        more_properties = TRUE,
+        event_properties = c("DESCRIPTION" = .$url[[1]],
+          "DTSTAMP" = dtstamp)))) %>%
     ungroup() %>%
     select(-id, -data) %>%
     unnest(ical) %>%
     ical() %>%
     rename(`DTSTART;VALUE=DATE` = DTSTART,
-           `DTEND;VALUE=DATE` = DTEND)
-
+      `DTEND;VALUE=DATE` = DTEND)
+  
   return(schedule_ics)
 }
